28177d5d9b058f5f6438afdcf0d7029ffbc43d9e
aymene01 <aymene01@hotmail.fr>
2023-08-16 16:01:19 +0200

    cleaning validation file

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -6,7 +6,7 @@ import { pipe } from 'fp-ts/lib/function'
 import { map } from 'fp-ts/lib/Either'
 import lift from './behavioral/lift'
 import { ERROR } from './constant'
-import applicativeValidation from './behavioral/applicative'
+import applicativeValidation from './behavioral/applicativeValidation'
 
 /* ↠ LOCAL NEEDED VALUES ↞ */
 
@@ -14,17 +14,9 @@ const PASSWORD_MIN_LENGTH = 6
 
 /* ↠ UTILITIES ↞ */
 
-/**
- * @description check if two string are equals
- */
-
 const equal = (s: string, t: string): Either<string, string> => {
   return s === t ? right(s) : left(ERROR.VALIDATION.USER.PASSWORD.MISMATCH)
 }
-
-/**
- * @description check the minimal lentgh needed
- */
 
 const minLength =
   (min: number) =>
@@ -32,16 +24,10 @@ const minLength =
     return s.length >= PASSWORD_MIN_LENGTH ? right(s) : left(ERROR.VALIDATION.USER.PASSWORD.MIN_LENGTH(min))
   }
 
-/**
- * @description check if a string contain at least one capitalize character
- */
 const oneCapital = (s: string): Either<string, string> => {
   return /[A-Z]/g.test(s) ? right(s) : left(ERROR.VALIDATION.USER.PASSWORD.ONE_CAPITAL)
 }
 
-/**
- * @description check if a string contain at least one number
- */
 const oneNumber = (s: string): Either<string, string> => {
   return /[0-9]/g.test(s) ? right(s) : left(ERROR.VALIDATION.USER.PASSWORD.ONE_NUMBER)
 }
@@ -55,9 +41,6 @@ const equalV = lift(equal)
 
 /* ↠ VALIDATORS ↞ */
 
-/**
- * @description validate a password using severals functions and stack errors as array
- */
 export const vPassword = (s: string, confirmedPassword: string): Either<NonEmptyArray<string>, string> => {
   return pipe(
     sequenceT(applicativeValidation)(equalV(s, confirmedPassword), minLengthV(s), oneCapitalV(s), oneNumberV(s)),
@@ -65,9 +48,6 @@ export const vPassword = (s: string, confirmedPassword: string): Either<NonEmpty
     map(() => s),
   )
 }
-/**
- * @description validate an email and stack errors as array
- */
 
 export const vEmail = (s: string): Either<NonEmptyArray<string>, string> => {
   return isemail.validate(s, { minDomainAtoms: 2 }) ? right(s) : left([ERROR.VALIDATION.USER.EMAIL.NOT_VALID])
