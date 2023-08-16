0c6ad086e2aac47c983259cabebe29fe0b7be6c2
aymene01 <aymene01@hotmail.fr>
2023-08-16 15:22:50 +0200

    add hello command

---
+++ src/bin.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { program } from './command'
+
+program.parse(process.argv)
--- src/command.ts
+++ src/command.ts
@@ -1,3 +1,14 @@
 import { Command } from 'commander'
 
 export const program = new Command()
+
+program
+  .command('hello <name>')
+  .option('-c, --capitalize')
+  .action((name: string, options) => {
+    const { capitalize } = options
+    const message = `hello ${name}`
+    const greeting = capitalize ? message.toUpperCase() : message
+
+    console.log(greeting)
+  })
