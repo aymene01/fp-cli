505c283bc1fec9cfc0387b98792b72d16e578469
aymene01 <aymene01@hotmail.fr>
2023-08-17 09:49:51 +0200

    refacto isSuccess

--- src/lib/isSuccess.ts
+++ src/lib/isSuccess.ts
@@ -1,5 +1,3 @@
 import * as E from 'fp-ts/Either'
 
-export const isSuccess = <T>(result: E.Either<Error, T>): result is E.Right<T> => {
-  return result._tag === 'Right'
-}
+export const isSuccess = <T>(result: E.Either<Error, T>): result is E.Right<T> => result._tag === 'Right'
